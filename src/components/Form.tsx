31569c05190f0d2a1f411e5b017e890958eb82d0
Sofia0101 <fornaroliforna@gmail.com>
2024-10-05 15:40:46 -0300

    :fire: using hook UseReducer

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,38 +1,80 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { Subs } from "../types";
 
 interface FormState{
-    nickname: string;
-    subMonths: number;
-    avatar: string;
-    description?: string;
+    inputValues: Subs
 }
 
 interface FormProps{
   onNewSub: (newSub: Subs) => void
 }
 
+
+const INITIAL_STATE = {
+  nickname: "",
+  subMonths: 2,
+  avatar: "",
+  description: "",
+}
+
+type FormReducerAction = {
+  type: "change_value",
+  payload: {
+    inputName: string,
+    inputValue: string
+  }
+} | { 
+    type: "clear"
+  }
+
+
+const formReducer = (state: FormState["inputValues"], action: FormReducerAction) => {
+  switch (action.type){
+    case "change_value":
+    const {inputName, inputValue} = action.payload
+    return {
+      ...state,
+      [inputName]: inputValue
+    }
+    case "clear":
+    return INITIAL_STATE 
+  } 
+}
+
+const useNewSubForm = ()  => {
+  return useReducer(formReducer, INITIAL_STATE)
+}
+
 const Form = ({onNewSub}: FormProps) => {
-  const [inputValues, setInputValues] = useState<FormState>({
-    nickname: "",
-    subMonths: 0,
-    avatar: "",
-    description: "",
-  });
+  const [inputValues, dispatch] = useNewSubForm()
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     onNewSub(inputValues)
+    handleClear()
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setInputValues({
-      ...inputValues,
-      [e.target.name]: e.target.value,
-    });
+    
+    const {name, value} = e.target
+
+    dispatch({
+      type: "change_value",
+      payload: {
+        inputName: name,
+        inputValue: value
+      }
+    })
   };
 
   // (target.name hace referencia a la key "name" de cada input, no al nickname)
+
+  const handleClear = () => {
+    dispatch({
+      type: "clear",
+     })
+  }
+
 
   return (
     <div>
@@ -65,7 +107,8 @@ const Form = ({onNewSub}: FormProps) => {
           placeholder="description"
         />
 
-        <button>Save new sub!</button>
+        <button type="submit">Save new sub!</button>
+        <button onClick={handleClear} type="button">Clear the form</button>
       </form>
     </div>
   );
